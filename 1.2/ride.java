9dcc52e92fa2c4e055d4f2d94afb98b492a1b661
LLLgoyour <lllgoyour@protonmail.com>
2021-08-24 21:38:27 -0400

    update

---
+++ 1.2/ride.java
@@ -0,0 +1,15 @@
+
+/*
+ID: lllgoyo2
+LANG: JAVA
+TASK: ride
+*/
+import java.io.*;
+import java.util.*;
+
+class ride {
+    public static void main(String[] args) throws Exception {
+        BufferedReader f = new BufferedReader(new FileReader("ride.in"));
+
+        }
+}
