230a267f9125dd8833dd01b987364fe061596d01
LLLgoyour <lllgoyour@protonmail.com>
2022-01-08 01:51:13 -0500

    🛠

---
+++ USACO_guide/week1/time_complexity/hw/Two_Tables/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+//https://codeforces.com/problemset/problem/1555/B
+
+
+public class Main {
+    static void main(String[] args) throws IOException
+        {
+            Kattio io = new Kattio("main");
+            
+        }
+
+        static class Kattio extends PrintWriter
+    {
+    }
+}
