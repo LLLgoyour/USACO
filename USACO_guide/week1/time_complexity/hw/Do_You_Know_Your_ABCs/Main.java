b80bb55e1bbdd8dce9e099226710e68d7a464bcc
LLLgoyour <lllgoyour@protonmail.com>
2022-01-15 01:10:28 -0500

    😕

--- USACO_guide/week1/time_complexity/hw/Do_You_Know_Your_ABCs/Main.java
+++ USACO_guide/week1/time_complexity/hw/Do_You_Know_Your_ABCs/Main.java
@@ -1,40 +1,22 @@
 import java.io.*;
+import java.util.*;
 
 //http://www.usaco.org/index.php?page=viewproblem2&cpid=1059&lang=zh
 
-/*
-Farmer John 的奶牛正在 "mooZ" 视频会议平台上举行每日集会。她们发明了一个简单的数字游戏，为会议增添一些乐趣。
-Elsie 有三个正整数 A、B 和 C（A≤B≤C）。这些数字是保密的，她不会直接透露给她的姐妹 Bessie。她告诉 Bessie 七个范围在 1…109 之间的整数（不一定各不相同），并宣称这是 A、B、C、A+B、B+C、C+A 和 A+B+C 的某种排列。
-
-给定这七个整数，请帮助 Bessie 求出 A、B 和 C。可以证明，答案是唯一的。
-
-输入格式（从终端/标准输入读入）：
-输入一行，包含七个空格分隔的整数。
-输出格式（输出至终端/标准输出）：
-输出 A、B 和 C，用空格分隔。
-输入样例：
-2 2 11 4 9 7 9
-输出样例：
-2 2 7
-测试点性质：
-测试点 2-3 满足 C≤50。
-测试点 4-10 没有额外限制。
-*/
 
 public class Main {
     static void main(String[] args) throws IOException
         {
-            Kattio io = new Kattio("main");
-            int a = io.nextInt();
-            int b = io.nextInt();
-            int c = io.nextInt();
-            int d = io.nextInt();
-            int e = io.nextInt();
-            int f = io.nextInt();
-            int g = io.nextInt();
-            
+            Kattio std = new Kattio(System.in);
+            int[] values = new int[7];
+			for (int i = 0; i < 7; i++) 
+				values[i] = std.nextInt();
+			Arrays.sort(values);
+			int c = values[6] - values[1] - values[0];
+			System.out.println(values[0]+" "+values[1]+" "+c);
         }
-
+		// 存入数组 取最大 然后取剩余三个最大 用sort方法排序
+		
         static class Kattio extends PrintWriter
     {
 		private BufferedReader r;
---
+++ USACO_guide/week1/time_complexity/hw/white_sheet/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new FileReader("Main.in"));
+        PrintWriter out = new PrintWriter(new FileWriter("Main.out"));
+        // TODO: 
+        int x1 = Integer.parseInt(br.readLine());
+        out.close();
+    }
+    
+}
