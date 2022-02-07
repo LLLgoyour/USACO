044242a6d78e36694e8e82dc0b8242f0874626dd
LLLgoyour <lllgoyour@protonmail.com>
2022-02-07 17:14:51 -0500

    Update Main.java

--- USACO/measuring_traffic/Main.java
+++ USACO/measuring_traffic/Main.java
@@ -5,15 +5,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new FileReader("traffic.in"));
         PrintWriter out = new PrintWriter(new FileWriter("traffic.out"));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        
 
         int N = br.readLine();
         for (int i = 0; i < N; N++)
         {
-            String Ramp = br.readLine();
-            int lowerBound = 
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String Ramp = st.nextToken(); // 闸道类型
+            int [] lowerBound = st.nextToken(); // 下限
+            int [i] higherBound = st.nextToken(); // 上限
         }
-        
+        /* 取上闸道车辆数量，取下闸道车辆数量
+         * 上闸道后第1公里取上限减去上闸道车辆
+         * 下闸道前1公里取下限减去下闸道上限，取上限减去下闸道上限
+         */
+
+        for ()
+        {
+
+            if (Ramp.equals("on")) 
+            {
+                
+            }
+        }
+        out.close();
         
         
 }
