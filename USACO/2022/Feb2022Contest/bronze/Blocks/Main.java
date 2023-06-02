7a75a458bc3a61129376ce6a0006a87a6babaece
LLLgoyour <lllgoyour@protonmail.com>
2023-06-02 20:26:25 +0800

    USACO22FebBronze

--- USACO/2022/Feb2022Contest/bronze/Blocks/Main.java
+++ USACO/2022/Feb2022Contest/bronze/Blocks/Main.java
@@ -1,21 +1,41 @@
-import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(in.readLine());
-        char[] s = new char[4];
-        for (int i = 1; i <= 4; i++)
-            s[i] = in.readLine().toCharArray();
-        for (int i = 1; i <= n; i++) {
-            String word = in.readLine();
-            for (int j = 0; j < 4; j++) {
-                for (int p = 0; p < word.length(); p++) {
+    static String[] a = new String[15];
+    static String s;
+    static boolean[] f = new boolean[15];
+    static boolean ans;
 
-                }
+    static void dfs(int dep, int x) {
+        if (dep == s.length()) {
+            ans |= (s.length() == x);
+            return;
+        }
+        for (int i = 1; i <= 4; i++) {
+            if (f[i])
+                continue;
+            if (a[i].indexOf(s.charAt(x)) != -1) {
+                f[i] = true;
+                dfs(dep + 1, x + 1);
+                f[i] = false;
             }
-            System.out.println(flag ? "YES" : "NO");
+            dfs(dep + 1, x);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        a[1] = in.next();
+        a[2] = in.next();
+        a[3] = in.next();
+        a[4] = in.next();
+        while (n-- != 0) {
+            s = in.next();
+            dfs(0, 0);
+            System.out.println(ans ? "YES" : "NO");
+            ans = false;
+            Arrays.fill(f, false);
         }
     }
 }
