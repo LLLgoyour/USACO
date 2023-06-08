41da993777ecc7f1856d5a20526a676544d8a88c
LLLgoyour <lllgoyour@protonmail.com>
2023-06-08 23:55:21 +0800

    Update Main.java

--- luogu/P2879/Main.java
+++ luogu/P2879/Main.java
@@ -6,12 +6,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String[] s = in.readLine().split(" ");
-        int n = s[0], i = s[1], h = s[2], r = s[3];
-        int[] pair = new int[2];
         HashMap<Integer, Integer> Pairs = new HashMap<Integer, Integer>();
+        int n = s[0], i = s[1], h = s[2], r = s[3];
+        Pairs.put(i, h); // maximum height
+        for (int j = 0; j < n; j++) {
+            if (j = i - 1) {
+                continue;
+            }
+            Pairs.put(j, h - 1);
+        }
+
         for (int j = 0; i < r; i++) {
             s = in.readLine().split(" ");
-            Pairs.put(s[0], s[1]);
+            Pairs.put(s[0], h - 1);
+            Pairs.put(s[1], h - 1);
         }
 
     }
