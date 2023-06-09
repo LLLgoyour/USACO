94d8a392f3bfd1b995d7e8260aedc317095db6ef
LLLgoyour <lllgoyour@protonmail.com>
2023-06-09 23:41:29 +0800

    Codeforces:1737A
    unfinished

---
+++ Codeforces/1737A/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(in.readLine());
+        int n = 0, k =0;
+        while(number>0){
+            String s = in.readLine().split(" ");
+            n = s[0];
+            k = s[1];
+            String letter = in.readLine();
+            
+            number--;
+        }
+    }
+}
+
